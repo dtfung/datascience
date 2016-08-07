5604117df0602e30a34478f8e4626d863eff9ae7
Donald Fung <donaldtfung@gmail.com>
2016-08-06 22:44:41 -0400

    docs: Renamed project folder

---
+++ Projects/Predict-Stock-Prices/predict.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug  6 22:16:58 2016
+
+@author: donaldfung
+"""
+
---
+++ Projects/Predict-Stock-Prices/settings.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug  6 22:09:17 2016
+
+@author: donaldfung
+"""
+
+""" STORE ALL FILE NAMES HERE """
+
+data_dir = "data"
+
+# parameters
+
+# API Key
+
+# API Endpoints
+
+
