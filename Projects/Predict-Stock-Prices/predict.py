864a38145f26c1149c95c9eb8edd051d497f7832
Donald Fung <donaldtfung@gmail.com>
2016-08-06 23:17:47 -0400

    feat: Add paths for a few variables
    included are: stock price, database, PE ratio, data folder values

--- Projects/Predict-Stock-Prices/predict.py
+++ Projects/Predict-Stock-Prices/predict.py
@@ -5,3 +5,5 @@ Created on Sat Aug  6 22:16:58 2016
 @author: donaldfung
 """
 
+import settings
+
--- Projects/Predict-Stock-Prices/settings.py
+++ Projects/Predict-Stock-Prices/settings.py
@@ -6,13 +6,40 @@ Created on Sat Aug  6 22:09:17 2016
 """
 
 """ STORE ALL FILE NAMES HERE """
+import key
 
+# data 
 data_dir = "data"
+
 
 # parameters
 
-# API Key
+# stock ticker
+ticker = "AAPL"
 
-# API Endpoints
+""" QUANDL DATABASES """
+
+""" Wiki EOD Stock Prices 
+End of day stock prices, dividends and splits for 3,000 US companies, 
+curated by the Quandl community and released into the public domain 
+"""
+wiki = "WIKI/"
+
+""" Core US Fundamentals Data
+
+7,000+ companies, point-in-time, inc/exc restatements, active/delisted,
+up to 11 years history, 101 indicators, expanding coverage, daily updates.
+"""
+sf1 = "SF1/"
+
+# API Key
+api_key = key.get_key() 
+
+""" QUANDL CODES """
+
+# stock price code
+stock_price_code = wiki + ticker
+# PE ratio code
+pe_ratio_code = sf1 + ticker + "_PE1_MRT"
 
 
