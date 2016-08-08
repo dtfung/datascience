1dd1b28db3b67c0e514b93c0466db3b4f2bce2cd
Donald Fung <donaldtfung@gmail.com>
2016-08-08 00:48:03 -0400

    feat:  Add functions
    Add function to normalize data
    Add function to fill missing values

--- Projects/Predict-Stock-Prices/prepare.py
+++ Projects/Predict-Stock-Prices/prepare.py
@@ -4,4 +4,16 @@ Created on Sat Aug  6 21:51:23 2016
 
 @author: donaldfung
 """
+import pandas as pd
+""" DATA PREPROCESSING """
 
+def normalize_data(df = None):
+    starting_price = df.ix[0, :]
+    df = df/starting_price 
+    return df
+
+def fill_missing_values(dataframe):
+    # Forward fill missing data
+    dataframe.fillna(method = "ffill", inplace = True)
+    dataframe.fillna(method = "bfill", inplace = True)
+    return dataframe
