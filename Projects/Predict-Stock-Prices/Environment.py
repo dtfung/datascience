002f62abb28c582c5b1cfa7f3a7ed734bb03916b
Donald Fung <donaldtfung@gmail.com>
2016-08-09 21:53:39 -0400

    feat: Combine features
    features include:
    Bollinger bands, volume, cumulative return, daily return,

--- Projects/Predict-Stock-Prices/Environment.py
+++ Projects/Predict-Stock-Prices/Environment.py
@@ -6,7 +6,9 @@ Created on Tue Aug  9 17:34:06 2016
 """
 import computations
 import predict
-
+import prepare
+import assemble
+import pandas as pd
 class Market():
     def __init__(self):
         self.state = None
@@ -15,10 +17,24 @@ class Market():
     def get_financials(self, data):
         financials = computations.Financials()
         n = 20 # look back n number of days / window
+        volume = data["Adj. Volume"]
         rolling_mean = financials.get_rolling_mean(data, n)# get rolling mean
         upper, lower = financials.get_bollinger_bands(data, n) # get bollinger bands
-        #self.learner.update(state)
-        cumulative_returns = financials.get_cumulative_returns(data)
-        adj_close_sma = financials.get_close_SMA_ratio(data, n)
+        daily_returns = financials.get_daily_returns(data) # get daily returns
+        cumulative_returns = financials.get_cumulative_returns(data) # get cumulative returns
+        adj_close_sma = financials.get_close_SMA_ratio(data, n) # get adj.close to SMA ratio
+        # add data to dataframe
+        df = data.copy()
+        # dictionary of features
+        features = {"Rolling_Mean":rolling_mean, "Upper":upper, "Lower":lower,"Daily_Returns":daily_returns,
+        "Cumulative_Returns":cumulative_returns, "Close_SMA":adj_close_sma}
+        for key, feature in features.items():
+            column = [key]
+            temp_df = feature.to_frame()
+            temp_df.columns = [column]
+            df = df.join(temp_df, how = "outer")
+    
+        
+    
         
         
