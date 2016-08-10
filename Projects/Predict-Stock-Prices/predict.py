fb125742f8edfb99c08c6320f351f64129c4bfdf
Donald Fung <donaldtfung@gmail.com>
2016-08-10 11:15:28 -0400

    feat: Add q-table lookup, save and choose action functionality
    Properties added to store state, action, rewards, alpha, gamma, epsilon
    Function added to choose optimal action in a given state
    Function added to get max Q
    Function added to update Q-table with new q values

--- Projects/Predict-Stock-Prices/predict.py
+++ Projects/Predict-Stock-Prices/predict.py
@@ -8,12 +8,20 @@ Created on Sat Aug  6 22:16:58 2016
 import settings
 import assemble
 import Environment
-
+import random
+    
 class Q_Learning():
     def __init__(self, data):
         # Initialize variables here
         self.prices = data["Adj. Close"]
-        self.count = 0
+        self.actions = ["buy", "hold", "sell"] 
+        self.lastState = None
+        self.lastAction = None
+        self.lastReward = None
+        self.qTable = {}
+        self.epsilon = 0.05 # exploration rate
+        self.alpha = 0.1 # learning rate
+        self.gamma = 0.1 # discount rate
         
     def update(self, state):
         """ Get new state from environment
@@ -21,10 +29,56 @@ class Q_Learning():
         input:  state = (volume, rolling_mean, upper, lower,  cumulative returns close_sma)
                 type = tuple
         """
+        # TODO: first move
+        if self.lastState is None: 
+            action = random.choice(self.actions) # random action chosen on first move
+            # TODO: calculate reward
+            """ for now, assume commission fees, dividend payouts aren't included """
+            reward = None
         
+        action = self.choose_action(state)
+        # TODO: calculate reward
+         """ for now, assume commission fees, dividend payouts aren't included """
+        reward = None
+        # TODO: update q-table with last state, action, reward and current state
         
-            
-            return
+        self.qLearn(self.lastState, self.lastAction, self.lastReward, self.state)
+        
+        self.lastState = self.state
+        self.lastAction = action
+        self.lastReward = reward   
+    
+    def choose_action(self, state):
+        q = [self.getQ(state, a) for a in self.actions]
+        maxQ = max(q)
+    
+        if random.random() < self.epsilon:
+        action = random.choice(self.actions)
+        else:
+            count = q.count(maxQ)
+            if count > 1:
+                best = [i for i in range(len(self.actions)) if q[i] == maxQ]
+                i = random.choice(best)
+                else:
+                    i = q.index(maxQ)
+
+                action = self.actions[i]
+        return action
+        
+    # Find the max state-action value in the current state and use it to update the Q table
+    def qLearn(self, lastState, lastAction, lastReward, state):
+        # get the max Q for the current state here
+        maxQnew = max([self.getQ(state, action) for action in self.actions])
+        # call method to update the Q table here
+        self.updateQ(lastState, lastAction, lastReward, maxQnew)
+    
+    def getQ(self, state, action):
+        return self.qTable.get((state, action), 0.0)
+    
+    def updateQ(self, state, action, reward, maxQnew):
+        # Update Q Values for the last state and action
+        oldValue = self.qTable.get((state, action), 0.0)
+        self.qTable[(state, action)] = oldValue + self.alpha * (reward + (self.gamma * maxQnew) - oldValue)
 
 def run():
     #   Get stock price data
