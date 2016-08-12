161a126776116883e7d356445bf5807134fdf4d5
Donald Fung <donaldtfung@gmail.com>
2016-08-12 14:32:23 -0400

    feat: Add parameters

--- Projects/Predict-SnP500/settings.py
+++ Projects/Predict-SnP500/settings.py
@@ -5,3 +5,7 @@ Created on Fri Aug 12 11:05:55 2016
 @author: donaldfung
 """
 
+filename = "Data/YAHOO-INDEX_GSPC.csv"
+timeframe = [5, 30, 365]
+features = ["SMA_30d","STD_30d","SMA30_STD30"]
+target = "Adjusted Close"
