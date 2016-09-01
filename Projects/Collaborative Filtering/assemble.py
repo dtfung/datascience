7b141a19c879c5a33ba764c039e22df7e0af65b3
Donald Fung <donaldtfung@gmail.com>
2016-08-31 22:24:10 -0400

    feat: File to read and preprocess data

---
+++ Projects/Collaborative Filtering/assemble.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 31 21:21:59 2016
+
+@author: donaldfung
+"""
+import settings
+
+class Data():
+    
+    def __init__(self):
+        # this is dummy data, so no preprocessing steps needed
+        self.user_data = settings.critics
