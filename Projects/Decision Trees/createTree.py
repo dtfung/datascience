b41aae989984b7c957d2163a5664d37ec05d230f
Donald Fung <donaldtfung@gmail.com>
2016-09-06 03:33:03 -0400

    feat: Calculate best split

---
+++ Projects/Decision Trees/createTree.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep  6 01:01:44 2016
+
+@author: donaldfung
+"""
+
+""" Decision Trees """
+import assemble
+import pandas as pd
+import numpy as np
+import math
+
+class DecisionTree():
+    def __init__(self, data):
+        self.data = data
+        
+    def calc_entropy(self, column):
+        counts = np.bincount(column)
+        probabilities = counts / float(len(column))
+        entropy = 0
+        for prob in probabilities:
+            if prob > 0:
+                entropy += prob * math.log(prob, 2)
+        return -entropy
+    
+    def calc_information_gain(self, split_name, target_name):
+        entropy_T = self.calc_entropy(self.data[target_name])
+        median = self.data[split_name].median()
+        left_split = self.data[self.data[split_name] <= median]
+        right_split = self.data[self.data[split_name] > median]
+        subset_entropies = 0
+        for subset in [left_split, right_split]:
+            prob = (subset.shape[0] / float(self.data.shape[0]))
+            subset_entropies += prob * self.calc_entropy(subset[target_name])
+        info_gain = entropy_T - subset_entropies
+        return info_gain
+        
+def main():
+    prep = assemble.Preprocessing()
+    prep.read_file()
+    prep.convert_categorical_variables()
+    data = prep.data
+    tree = DecisionTree(data)
+    information_gains = []
+    columns = ["age", "workclass", "education_num", 
+               "marital_status", "occupation", "relationship", 
+               "race", "sex", "hours_per_week", "native_country"]
+    for col in columns:
+        info_gain = tree.calc_information_gain(split_name = col, target_name = "high_income")
+        information_gains.append(info_gain)
+    max_info_gain = information_gains.index(max(information_gains))
+    best_split = columns[max_info_gain]
+          
+if __name__ == "__main__":
+    main()
+
+    
+    
---
+++ Projects/Decision Trees/settings.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep  6 01:05:24 2016
+
+@author: donaldfung
+"""
+
+filename = "https://archive.ics.uci.edu/ml/machine-learning-databases/adult/adult.data"
