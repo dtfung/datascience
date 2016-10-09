179eeb0819bf40941095d39d7529d44ada1f82e0
Donald Fung <donaldtfung@gmail.com>
2016-10-09 13:35:53 -0400

    feat: make call to return stock price data

--- Projects/Predict-Stock-Prices/predict.py
+++ Projects/Predict-Stock-Prices/predict.py
@@ -6,3 +6,16 @@ Created on Fri Aug 12 12:05:10 2016
 
 Contributors: Deepak Mahtani, Blayne Chong, Donald Fung
 """
+
+import assemble
+import settings
+import pandas as pd
+
+if __name__ == "__main__":
+    
+    # initialize company class
+    company = assemble.CompanyData(settings.company_ticker)
+    
+    # preprocess data
+    df = (pd.DataFrame()
+            .pipe(company.get_wiki_data()))
