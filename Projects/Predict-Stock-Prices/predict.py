cae6721d33cf1a776bc01b4a63bcdf3cb62f0758
Donald Fung <donaldtfung@gmail.com>
2016-10-13 16:06:29 -0400

    feat: Make calls to preprocess data

--- Projects/Predict-Stock-Prices/predict.py
+++ Projects/Predict-Stock-Prices/predict.py
@@ -8,7 +8,6 @@ Contributors: Deepak Mahtani, Blayne Chong, Donald Fung
 """
 
 import assemble
-import settings
 import pandas as pd
 
 if __name__ == "__main__":
@@ -16,6 +15,26 @@ if __name__ == "__main__":
     # initialize company class
     company = assemble.CompanyData()
     
-    # preprocess data
-    df = (pd.DataFrame()
-            .pipe(company.get_wiki_data()))
+    # get price data
+    price_data = company.get_wiki_data()
+    
+    # get fundamentals
+    fundamentals_data = company.get_fundamentals()
+    
+    # rename columns
+    fundamentals = (fundamentals_data.pipe(company.rename_columns))
+    
+    # date of first reported quarterly
+    first_date = fundamentals.index[0]
+    
+    # combine dataframes
+    result = (pd.concat([price_data, fundamentals], axis = 1)
+                .pipe(company.remove_rows, first_date)
+                .fillna(method = 'ffill')
+                .pipe(company.calc_pe_ratio)
+                .pipe(company.calc_book_value_pershare)
+                .pipe(company.calc_pb_ratio)
+                .pipe(company.calc_ps_ratio)
+                .pipe(company.calc_pcashflow_ratio)
+                )
+                
