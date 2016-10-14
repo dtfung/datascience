f7ff90e8eed5bec657503b27779caa7721ba0784
Donald Fung <donaldtfung@gmail.com>
2016-10-14 03:29:12 -0400

    feat: Normalize data

--- Projects/Predict-Stock-Prices/predict.py
+++ Projects/Predict-Stock-Prices/predict.py
@@ -8,6 +8,7 @@ Contributors: Deepak Mahtani, Blayne Chong, Donald Fung
 """
 
 import assemble
+import settings
 import pandas as pd
 
 def preprocess_data():
@@ -35,9 +36,7 @@ def preprocess_data():
                 .pipe(company.calc_book_value_pershare)
                 .pipe(company.calc_pb_ratio)
                 .pipe(company.calc_ps_ratio)
-                .pipe(company.calc_pcashflow_ratio)
-                .pipe(company.normalize)
-                )
+                .pipe(company.calc_pcashflow_ratio))
     return df
     
 def partition(df):
@@ -48,13 +47,42 @@ def partition(df):
     train = df.iloc[:train_size]
     validation = df.iloc[train_size:train_size + test_size]
     test = df.iloc[-test_size:]
-    return train, validation, test
+
+    # target label valus
+    y_train = train.loc[:, ("Adj. Close")]
+    y_val = validation.loc[:, ("Adj. Close")]
+    y_test = test.loc[:, ("Adj. Close")]
+                      
+    # feature data
+    X_train = train.drop("Adj. Close", axis = 1)
+    X_val = validation.drop("Adj. Close", axis = 1)
+    X_test = test.drop("Adj. Close", axis = 1)
+    return X_train, y_train, X_val, y_val, X_test, y_test
+    
+def feature_scale(X_train, X_val, X_test):
+    "Normalize all columns"""
+    from sklearn.preprocessing import MinMaxScaler
+    mms = MinMaxScaler()
+    
+    X_train_std = mms.fit_transform(X_train)
+    X_val_std = mms.fit_transform(X_val)
+    X_test_std = mms.fit_transform(X_test)
+    return X_train_std, X_val_std, X_test_std
     
     
 def predict(df):
     
     # partition data
-    train, validation, test = partition(df)
+    X_train, y_train, X_val, y_val, X_test, y_test = partition(df)
+    
+    # normalize features
+    X_train_std, X_val_std, X_test_std = feature_scale(X_train, X_val, X_test)
+     
+    
+    # get time frame
+    time_frame = settings.time_frame
+    print time_frame
+    
 
 if __name__ == "__main__":
     
