6e21a0738d9a097c98bac258d24a38883db017ab
Donald Fung <donaldtfung@gmail.com>
2017-02-02 10:27:21 -0500

    refactor: remove unused code

--- Projects/Data Science Bowl 2017/src/data/make_dataset.py
+++ Projects/Data Science Bowl 2017/src/data/make_dataset.py
@@ -2,7 +2,6 @@
 import os
 import click
 import logging
-#from dotenv import find_dotenv, load_dotenv
 
 @click.command()
 @click.argument('input_filepath', type = click.Path(exists=True))
@@ -27,10 +26,4 @@ if __name__ == '__main__':
     log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
     logging.basicConfig(level=logging.INFO, format=log_fmt)
 
-    # not used in this stub but often useful for finding various files
-    project_dir = os.path.join(os.path.dirname(__file__), os.pardir, os.pardir)
-
-    # find .env automagically by walking up directories until it's found, then
-    # load up the .env entries as environment variables
-    #load_dotenv(find_dotenv())
     main()
