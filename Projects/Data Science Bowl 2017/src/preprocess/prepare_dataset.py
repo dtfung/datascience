3ee826bee94711add5b58bd28d4a43c7205077f1
Donald Fung <donaldtfung@gmail.com>
2017-02-23 10:17:39 -0500

    feat: Add functions to preprocess the images

--- Projects/Data Science Bowl 2017/src/preprocess/prepare_dataset.py
+++ Projects/Data Science Bowl 2017/src/preprocess/prepare_dataset.py
@@ -1,8 +1,11 @@
 # -*- coding: utf-8 -*-
 """
 @author: donald fung
-"""
 
+Credit to Guido Zuidhof for providing a tutorial on how to preprocess the data for this challenge.
+Link to tutorial below:
+https://www.kaggle.com/gzuidhof/data-science-bowl-2017/full-preprocessing-tutorial
+"""
 import os
 import time
 import numpy as np
@@ -11,9 +14,13 @@ import pydicom as dicom
 import scipy.ndimage
 from skimage import measure, morphology
 from mpl_toolkits.mplot3d.art3d import Poly3DCollection
+
+STAGE1_INPUTS = 'data/raw/image_data/stage1/'
+STAGE1_LABELS = 'data/raw/stage1_labels.csv'
+
+STAGE1_INPUTS_PROCESSED = # TODO: specify folder to save processed images
     
 def load_scans(path):
-    print(path)
     """Reads data files and returns a list of Pandas dataframes"""
     slices = [dicom.read_file(path + '/' + s) for s in os.listdir(path)]
     slices.sort(key = lambda x: int(x.ImagePositionPatient[2]))
@@ -25,25 +32,128 @@ def load_scans(path):
         s.SliceThickness = slice_thickness
     return slices
 
+def get_pixels_hu(slices):
+    image = np.stack([s.pixel_array for s in slices])
+    # Convert to int16 (from sometimes int16)
+    image = image.astype(np.int16)
+
+    # Set outside-of-scan pixels to 0
+    # The intercept is usually -1024, so air is approximately 0
+    image[image == -2000] = 0
+    
+    # Convert to Hounsfield units (HU)
+    for slice_number in range(len(slices)):
+        intercept = slices[slice_number].RescaleIntercept
+        slope = slices[slice_number].RescaleSlope
+        if slope != 1:
+            image[slice_number] = slope * image[slice_number].astype(np.float64)
+            image[slice_number] = image[slice_number].astype(np.int16)
+        image[slice_number] += np.int16(intercept)
+    return np.array(image, dtype=np.int16)
+
+def resample(image, scan, new_spacing=[1,1,1]):
+    # Determine current pixel spacing
+    spacing = np.array([scan[0].SliceThickness] + scan[0].PixelSpacing, dtype=np.float32)
+    resize_factor = spacing / new_spacing
+    new_real_shape = image.shape * resize_factor
+    new_shape = np.round(new_real_shape)
+    real_resize_factor = new_shape / image.shape
+    new_spacing = spacing / real_resize_factor
+    image = scipy.ndimage.interpolation.zoom(image, real_resize_factor, mode='nearest')
+    return image, new_spacing
+
+def largest_label_volume(im, bg=-1):
+    vals, counts = np.unique(im, return_counts=True)
+
+    counts = counts[vals != bg]
+    vals = vals[vals != bg]
+
+    if len(counts) > 0:
+        return vals[np.argmax(counts)]
+    else:
+        return None
+
+def segment_lung_mask(image, fill_lung_structures=True):
+    # not actually binary, but 1 and 2. 
+    # 0 is treated as background, which we do not want
+    binary_image = np.array(image > -320, dtype=np.int8)+1
+    labels = measure.label(binary_image)
+    
+    # Pick the pixel in the very corner to determine which label is air.
+    # Improvement: Pick multiple background labels from around the patient
+    # More resistant to "trays" on which the patient lays cutting the air 
+    # around the person in half
+    background_label = labels[0,0,0]
+    
+    #Fill the air around the person
+    binary_image[background_label == labels] = 2
+    
+    # Method of filling the lung structures (that is superior to something like 
+    # morphological closing)
+    if fill_lung_structures:
+        # For every slice we determine the largest solid structure
+        for i, axial_slice in enumerate(binary_image):
+            axial_slice = axial_slice - 1
+            labeling = measure.label(axial_slice)
+            l_max = largest_label_volume(labeling, bg=0)
+
+            #This slice contains some lung
+            if l_max is not None: 
+                binary_image[i][labeling != l_max] = 1
+
+    #Make the image actual binary
+    binary_image -= 1 
+
+    # Invert it, lungs are now 1
+    binary_image = 1-binary_image 
+
+    # Remove other air pockets insided body
+    labels = measure.label(binary_image, background=0)
+    l_max = largest_label_volume(labels, bg=0)
+
+    if l_max is not None: # There are air pockets
+        binary_image[labels != l_max] = 0
+    return binary_image
+
 def count_scans(filepath, num_patients):
-    """This function finds out how many unique scans exist for each patient.  I included a variable, num_patients,
-    to specify the number of patients that we are interested in"""
+    """This function finds out how many unique scans exist for each patient.  
+    I included a variable, num_patients, to specify the number of patients 
+    that we are interested in"""
     for d in os.listdir(filepath)[:num_patients]:
         print("Patient '{}' has {} scans".format(d, len(os.listdir(filepath + d))))
 
 def run():
-    """ Iterate over each image, and then apply a series of steps as outlined below:
+    """Iterate over each image, and then apply a series of steps as outlined below:
     1). Load scans and add calculate slice thickness
-    2). Convert pixel values to Hounsefield Units
+    2). Convert pixel values to Hounsefield Units(HU)
     3). Resampling
+    4). Lung Segmentation
+
+    Returns 
+    -------
+    ndarray
+        The scan 
     """
     # list of folders in directory
     patients = os.listdir(STAGE1_INPUTS)
     patients.sort
 
     # iterate over and preprocess scans for each patient
-    for patient in patients:
-        # TODO: load scans
-    
+    for patient in patients[:2]:
+        # get folder path
+        path = STAGE1_INPUTS + patient
+
+        # load scan & calculate thickness
+        scan = load_scans(path)
+
+        # convert pixel values to Hounsefield Units(HU)
+        hu_slices = get_pixels_hu(scan)
+
+        # resampling
+        pix_resampled, spacing = resample(hu_slices, scan, [1,1,1])
+
+        # lung segmentation
+        segmented_lungs_fill = segment_lung_mask(pix_resampled, True)
+        
 if __name__=="__main__":
     run()   
